6055e05c1e0c9f67ddddc7013e4b652367512dcb
hank5171 <hank850902@gmail.com>
2025-04-17 15:02:29 +0800

    add 0417

--- JavaWeb/src/main/java/servlet/LotteryServlet.java
+++ JavaWeb/src/main/java/servlet/LotteryServlet.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.Random;
 import java.util.Set;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -16,6 +17,7 @@ public class LotteryServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 1. 生成樂透號嗎
 		Random rs = new Random(); //隨機數物件
 		Set<Integer> numbers = new LinkedHashSet<>(); //放樂透號碼的容器
 		while (numbers.size() < 5) {
@@ -23,6 +25,13 @@ public class LotteryServlet extends HttpServlet {
 			numbers.add(number);
 		}
 		// 透過 resp 將資料顯示在瀏覽器
-		resp.getWriter().print(numbers);
+		//resp.getWriter().print(numbers);
+		// 2. 生成調度氣(分派器)
+		RequestDispatcher rd = req.getRequestDispatcher("/lottery2.jsp");
+		// 3. 傳遞樂透號嗎
+		req.setAttribute("numbers", numbers); // 裝配要傳遞的內容
+		rd.forward(req, resp);
+
+		
 	}
 }
