7cdc8f17ddfbbd0d88ad9e03b328708af0628953
hank5171 <hank850902@gmail.com>
2025-05-07 10:53:44 +0800

    0507 add SpringBoot

---
+++ springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+		return "Hello " + new Date();
+	}
+	
+	@GetMapping("/hi")
+	@ResponseBody
+	public String hi() {
+		return "Hi " + new Date();
+	}
+}
