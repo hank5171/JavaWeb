9b9b6de9e1739a76d877563d1bd1a8d16552817d
hank5171 <hank850902@gmail.com>
2025-06-09 10:28:43 +0800

    0609 aop Proxy add

---
+++ springboot-aop/src/main/java/com/example/demo/proxy/Man.java
@@ -0,0 +1,16 @@
+package com.example.demo.proxy;
+
+public class Man implements Person {
+
+	@Override
+	public void work() {
+		// 公用邏輯
+		//System.out.println("出門戴口罩");
+		
+		// 業務邏輯
+		System.out.println("Man 上班工作");
+		
+		// 公用邏輯
+		//System.out.println("回家脫口罩");
+	}
+}
---
+++ springboot-aop/src/main/java/com/example/demo/proxy/PersonProxy.java
@@ -0,0 +1,30 @@
+package com.example.demo.proxy;
+
+// 靜態代理
+// 用來執行/調用被代理者的"業務"邏輯與實現"公用"邏輯
+public class PersonProxy implements Person{
+
+	// 代理對象(被代理者)
+	private Person person;
+	
+	public PersonProxy(Person person) {
+		this.person = person;
+	}
+
+	@Override
+	public void work() {
+		// 實現公用邏輯-前置通知
+		System.out.println("出門戴口罩");
+		System.out.println("量體溫");
+		
+		// 調用業務邏輯
+		try {
+			person.work();
+		} catch (Exception e) {
+			System.err.print("工作中發生例外:" + e);
+		} finally {
+			// 實現公用邏輯-後置通知
+			System.out.println("回家脫口罩");
+		}
+	}
+}
